ffb822d647ec106e9bb3c30fd719e61bbe12056d
juaneins_uio <ballesterosjg@gmail.com>
2024-02-13 11:26:47 -0500

    normalizacion

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,49 @@
-import { reactive } from 'vue';
-
-const store = reactive({
-  username: 'connor.jhon',
-  updateUsername(username) {
-    this.username = username;
+const store = {
+  profile: {
+    username: '',
   },
-});
-
-export default store;
+  contacts: [
+    {
+      id: 1,
+      username: 'User 1',
+    },
+    {
+      id: 2,
+      username: 'User 2',
+    },
+  ],
+  messages: [
+    {
+      id: 1,
+      msg: 'Hola',
+      author: 1,
+    },
+    {
+      id: 2,
+      msg: 'Hola',
+      author: 2,
+    },
+    {
+      id: 3,
+      msg: 'Hola',
+      author: 1,
+    },
+    {
+      id: 4,
+      msg: 'Hola',
+      author: 2,
+    },
+  ],
+  channels: [
+    {
+      id: 1,
+      name: 'canal 1',
+      messages: [1, 2],
+    },
+    {
+      id: 2,
+      name: 'canal 2',
+      messages: [3, 4],
+    },
+  ],
+};
